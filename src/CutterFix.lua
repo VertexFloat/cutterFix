57d20caec4a0dd308c4a5fc5bdccefb8a4157054
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2023-06-04 17:20:19 +0200

    Clean-up code

--- src/CutterFix.lua
+++ src/CutterFix.lua
@@ -1,28 +1,20 @@
 -- @author: 4c65736975, All Rights Reserved
--- @version: 1.0.0.1, 17/08/2022
+-- @version: 1.0.0.1, 17|08|2022
 -- @filename: CutterFix.lua
 
--- Changelog (1.0.0.1) :
---
+-- Changelog (1.0.0.1):
 -- improved and more clearly code
-
-local overwritten = false
 
 function overwriteGameFunctions()
 	local function startThreshing() end
-
 	local function stopThreshing() end
 
-	if not overwritten then
-		overwritten = true
+	Combine['startThreshing'] = function (...)
+		return startThreshing(Combine['startThreshing'], ...)
+	end
 
-		Combine["startThreshing"] = function (...)
-			return startThreshing(Combine["startThreshing"], ...)
-		end
-
-		Combine["stopThreshing"] = function (...)
-			return stopThreshing(Combine["stopThreshing"], ...)
-		end
+	Combine['stopThreshing'] = function (...)
+		return stopThreshing(Combine['stopThreshing'], ...)
 	end
 end
 
